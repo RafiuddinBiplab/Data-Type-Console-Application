b448e4739588fe58ca712ea11b9e2a7596289c1b
ramcste <lucky.guyram@gmail.com>
2015-05-13 17:33:41 +0600

    Using different data type in a console application

---
+++ DataTypeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aNumber = 140;
+            double bNumbeer = 10.4;
+            char a = 'r';
+            string aText = "SEIP Project";
+            bool checker = false;
+            Console.WriteLine(aNumber+bNumbeer);
+            Console.WriteLine(checker);
+            Console.ReadKey();
+
+
+        }
+    }
+}
